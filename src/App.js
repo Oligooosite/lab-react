fbacf521bbbd740c2167fe9a3988f88f911f4e41
SANPONG EIANLHANG <sanpong94@gmail.com>
2022-08-19 16:18:57 +0700

    import WordCard to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import CharacterCard from './CharacterCard';
-
+import WordCard from './WordCard';
 const word = "Hello"
 function App() {
  return (
