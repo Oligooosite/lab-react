8868ed9c13c6fbeafd3f5d8bfe97caaa598a9ee6
SANPONG EIANLHANG <sanpong94@gmail.com>
2022-08-19 16:02:03 +0700

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+ return (
+ <div>
+ Hello World
+ </div>
+ );
 }
-
 export default App;
